f54d8599f58b12d1cc1b6755211608a2b0b42b9f
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-18 19:44:10 +0530

    change

--- src/features/auth/pages/Register.jsx
+++ src/features/auth/pages/Register.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      <h1>register</h1>
+    <main>
+    <div className='form-container'>
+        <h1>Register</h1>
+        <form  className='form'>
+            <input type="text" name="username" placeholder='Enter username' />
+            <input type="text" name="password" placeholder='Enter password'/>
+            <button>register</button>
+        </form>
     </div>
+   </main>
   )
 }
 
