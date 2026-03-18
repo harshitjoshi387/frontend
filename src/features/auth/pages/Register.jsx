711599ee1c2f8372c337b994d44e8ae9c7fc9fbd
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-18 20:45:03 +0530

    Add axios dependency and implement registration form submission

--- src/features/auth/pages/Register.jsx
+++ src/features/auth/pages/Register.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+  const [username, setusername] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post('http://localhost:3000/api/auth/register',{
+      username,
+      email,
+      password
+    })
+    .then(res=>{
+      console.log(res);
+      
+    })
+    
+  }
   return (
     <main>
     <div className='form-container'>
         <h1>Register</h1>
-        <form  className='form'>
-            <input type="text" name="username" placeholder='Enter username' />
-            <input type="email" name='email' placeholder='Enter  email' />
-            <input type="text" name="password" placeholder='Enter password'/>
+        <form onSubmit={handleSubmit} className='form' >
+            <input onInput={(e)=>{setusername(e.target.value)}} 
+            type="text" name="username" placeholder='Enter username' />
+            <input onInput={(e)=>{setusername(e.target.value)}} 
+             type="email" name='email' placeholder='Enter  email' />
+            <input onInput={(e)=>{setusername(e.target.value)}} 
+             type="text" name="password" placeholder='Enter password'/>
             <button>register</button>
         </form>
 
